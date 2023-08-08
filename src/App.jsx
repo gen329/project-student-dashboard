2f2d25c1038a6017e235b2d05431e5c4dde28de2
Genesis Lara <genesislara@pursuit.org>
2023-08-07 20:30:16 -0400

    setting up

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,19 @@
+import "./App.css";
+import studentData from "./data.json"
+import AllStudents from "./Components/StudentCards.jsx";
+import CohortList from "./Components/CohortList.jsx"
+import { useState } from "react";
+
 
 function App() {
+  const [student, setStudent] = useState(studentData)
+  const [cohort, setCohort] = useState([])
+  
   return (
     <div>
       <h1>Student Dashboard</h1>
+      <AllStudents />
+      <CohortList />
     </div>
   );
 }
