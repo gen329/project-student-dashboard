f037b5fdfc537047d43f0d507ed20e25a207ba16
Genesis Lara <genesislara@pursuit.org>
2023-09-28 16:04:04 -0400

    works on cohort list

--- src/Components/CohortList.jsx
+++ src/Components/CohortList.jsx
@@ -3,18 +3,27 @@ import CohortCard from "./CohortCard.jsx";
 import studentData from "../data/data.json";
 
 const CohortList = ({ selectedCohortSetter }) => {
-  const cohortList = [...new Set(studentData.map(student => student.cohort.cohortCode))]
-    .map(cohort => {
-      const season = cohort.slice(0, -4);
-      const year = cohort.slice(-4);
-      return `${season} ${year}`
-    });
+  try {
+    const cohortList = [...new Set(studentData.map(student => student.cohort.cohortCode))]
+      .map(cohort => {
+        const season = cohort.slice(0, -4);
+        const year = cohort.slice(-4);
+        return `${season} ${year}`
+      });
 
-  return (
-    <div className="CohortList">
-      {cohortList.map(cohort => <CohortCard cohort={cohort} selectedCohortSetter={selectedCohortSetter} />)}
-    </div>
-  );
-}
+    return (
+      <div className="CohortList">
+        {cohortList.map(cohort => <CohortCard cohort={cohort} selectedCohortSetter={selectedCohortSetter} />)}
+      </div>
+    );
+  } catch (error) {
+    console.error("An error occured while processing student data", error)
+    return (
+      <div className="CohortList">
+        <p>Error: Unable to load</p>
+      </div>
+    );
+  }
+};
 
 export default CohortList;
